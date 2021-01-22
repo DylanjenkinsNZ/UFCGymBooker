cea7f9ea4e54aaba5077c3421e3629d4903a638b
DylanjenkinsNZ <34000305+DylanjenkinsNZ@users.noreply.github.com>
2021-01-23 00:15:17 +0400

    Create boxingbook.sh

---
+++ boxingbook.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# As of 21-01-2021 this script is working
+# In future this script will check to see if the class is available and if so book
+
+# Read day to book
+echo "Enter date for booking e.g. 2021-01-15"
+read target
+
+# deprecated
+# calculate difference in times, add number of sec. in day and mod by same
+# sec_until=5800
+# let mins_until=$sec_until/60
+
+
+# Fetch timetable
+echo "curling time table for $target"
+timetable=$(curl 'https://ufcgymjbr.gymtour.app/ufc-hmvc/mobile/v1/class-session?date='+$target+'&user_id=4945')
+
+# Determine session ID of class
+session_id=`grep -o '.*BOXING' <<< $timetable | tail -c 21 | cut -c 1-5`
+echo "Session ID for Boxing on $target is $session_id"
+
+
+#TODO Check if class has free place
+#TODO Check if able to book due to time limit
+
+# snooze loop
+# sleep until 00:01 Dubai time (15:00 EDT), doesn't account for daylight savings
+echo "Starting sleep timer until 00:01"
+while [ $(date +%H:%M) != "15:01" ]; do sleep 1; done
+
+# book class
+(curl 'https://ufcgymjbr.gymtour.app/ufc-hmvc/mobile/v1/subscribe?Class%5Buser_id%5D=1398&Class%5Bsession_id%5D='+$session_id'')
